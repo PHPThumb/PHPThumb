e787a04cb737cc098c8d9642d01409c36054f4ab
Mario Gomez <marioquartz@gmail.com>
2022-06-05 11:27:17 +0200

    Webp support (#3)
    * In the test there are jpg files. And this fix a problem with them appear to be modified.
    
    * Revert "In the test there are jpg files. And this fix a problem with them appear to be modified."
    
    This reverts commit 6a0d38d11fda82bcd482964f03661ea4d302fe1f.
    
    * Webp support and test that confirms that the resulting image is resized.

--- tests/PHPThumb/Tests/GDTest.php
+++ tests/PHPThumb/Tests/GDTest.php
@@ -8,12 +8,14 @@ class GDTest extends \PHPUnit\Framework\TestCase
 	protected $gif;
 	protected $jpg;
 	protected $png;
+	protected $webp;
 
 	protected function setUp():void
 	{
 		$this->gif = new GD(__DIR__ . '/../../resources/test.gif');
 		$this->jpg = new GD(__DIR__ . '/../../resources/test.jpg');
 		$this->png = new GD(__DIR__ . '/../../resources/test.png');
+		$this->webp = new GD(__DIR__.'/../../resources/test.webp');
 	}
 
 	public function testLoadFileTypes()
@@ -21,6 +23,7 @@ class GDTest extends \PHPUnit\Framework\TestCase
 		self::assertSame('GIF', $this->gif->getFormat());
 		self::assertSame('JPG', $this->jpg->getFormat());
 		self::assertSame('PNG', $this->png->getFormat());
+		self::assertSame('WEBP', $this->webp->getFormat());
 	}
 
 	/**
@@ -32,5 +35,19 @@ class GDTest extends \PHPUnit\Framework\TestCase
 		$testImage = new GD(__DIR__ . '/../../resources/test.gif');
 		unset($testImage);
 		self::assertSame(false, isset($testImage));
+	}
+
+	/**
+	 * This test first resize a webp image and then save it in a temp file.
+	 * Load the image file and test if the resulting image have a width of 200 px.
+	 */
+	public function testWebp()
+	{
+		$this->webp->adaptiveResize(200, 200);
+		$tempFile = __DIR__. '/../../resources/resize.webp';
+		fwrite(fopen($tempFile, 'w'), $this->webp->getImageAsString());
+		$testing = new GD($tempFile);
+		self::assertSame(200, $testing->getCurrentDimensions()['width']);
+		unlink($tempFile);
 	}
 }
